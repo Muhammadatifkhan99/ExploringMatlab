db987aa38acd42ff29d5f4d37e81bf6de4031c19
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2023-02-14 19:19:47 +0500

    dealing with nested if else statements

---
+++ nestedifelsestatements.m
@@ -0,0 +1,16 @@
+a = 10;
+b = 2;
+c = 3;
+if (a > b)
+		if (a > c)
+			disp("a is the greatest")
+		else
+			disp("c is the greatest")
+endif
+else
+		if (b > c)
+			disp("b is the greatest")
+			else 
+			disp("c is the greatest")
+endif
+endif
