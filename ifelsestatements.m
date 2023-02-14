183cd94f2c92babc2872af109de8ae896fe31585
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2023-02-14 19:18:41 +0500

    practice with if else statement

---
+++ ifelsestatements.m
@@ -0,0 +1,16 @@
+%a = 1 
+%if a > 10
+% disp("a is greater then 10")
+%else 
+%  disp ("a is not greater then 10")
+%endif
+
+
+
+%%printing even and odd numbers
+a = input ("Enter a Number:")
+if mod(a,2) == 0
+  disp("is even")
+else 
+  disp(" is odd")
+endif
